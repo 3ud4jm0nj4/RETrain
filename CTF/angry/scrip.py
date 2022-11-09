016c126afd469e63481efc8320fb7e3ae9e6f0cc
3ud4jm0nj4 <tranduynam123@outlook.com>
2022-11-09 22:51:26 +0700

    ctf

---
+++ CTF/angry/scrip.py
@@ -0,0 +1,23 @@
+import angr
+from IPython import embed
+
+project = angr.Project("./angry", main_opts={'base_addr': 0}, auto_load_libs=False)
+
+
+# Start in main()
+initial_state = project.factory.entry_state()
+print(initial_state)
+# Start simulation
+sm = project.factory.simgr(initial_state)
+
+# Find the way yo reach the good address
+good_address = 0x000013BA
+
+# Avoiding this address
+avoid_address = 0x000013C8
+sm.explore(find=good_address, avoid=avoid_address)
+
+embed()
+
+# found = sm.found[0]
+# found.posix.dumps(0)
---
+++ CheatSheet.md
@@ -0,0 +1,5 @@
+# IDA
+tips:
+https://swarm.ptsecurity.com/ida-pro-tips/
+1.Alt+T: Text search
+2.Shift+E: Export Data
