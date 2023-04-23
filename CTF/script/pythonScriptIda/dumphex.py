555d41bfde3916cd623a017d91cd77a1a90497b1
Tran Duy Nam <83863539+3ud4jm0nj4@users.noreply.github.com>
2023-04-23 16:05:30 +0700

    up

--- CTF/script/pythonScriptIda/dumphex.py
+++ CTF/script/pythonScriptIda/dumphex.py
@@ -1,3 +1,10 @@
 with open("data.bin", "wb") as f:
     data = idc.get_bytes(addr,size)
     f.write(data)
+
+with open("data.bin", "wb") as f:
+    start_addr = 0x00000000023A9060
+    end_addr = 0x00000000023AB8C7
+    for addr in range(start_addr, end_addr+8, 8):
+        data = idc.get_qword(addr)
+        f.write(data.to_bytes(8, byteorder='little'))
